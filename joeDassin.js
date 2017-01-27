dafc60d6778950cc60e69177f1dcbd8e83bd05dc
Anthony Pilloud <pilloud.anthony@gmail.com>
2017-01-27 10:09:13 +0100

    delete while testing

--- joeDassin.js
+++ joeDassin.js
@@ -5,16 +5,9 @@ var isset = function isset () {
     var indefini;
     
     if (taille_arg === 0) {
-        throw new Error('Empty isset')
+        return false;
     }
     
-    /*while (i !== taille_arg) {
-        if (argument[i] === indefini || argument[i] === null) {
-            return false
-        }
-        i++
-    }
-    */
     for (var i=0 ; i<argument.length ; i++) {
         if (argument[i] === indefini || argument[i] === null) {
             return false
